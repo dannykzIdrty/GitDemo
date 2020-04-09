a8b19c77a8394f6c6655bef8cec411b1fd2973e6
dannykzIdrty <63408778+dannykzIdrty@users.noreply.github.com>
2020-04-09 23:18:36 +0800

    version 3 corrected grammer

--- repeatable+calculator.py
+++ repeatable+calculator.py
@@ -1,11 +1,11 @@
-print ("It is an calculator , calculate variable a and variable b")
-print ("Input the function and calculate with two variables followed by instruction")
+print("It is an calculator , calculate variable a and variable b")
+print("Input the function and calculate with two variables followed by instruction")
 
-while True: 
+while True:
 	try:
-		
+
 		a = input("\ninput a=")
-		
+
 		f = input("\nFunction list:" +
 		"\naddition input:+" +
 		"\nsubtraction input: -"+
@@ -15,40 +15,41 @@ while True:
 		"\nsquare root input: sqrt"+
 		"\n\nPlease input"+
 		"\n\ninput function=")
+
 		if f =="sqrt":
-			print ("only the result of square root of a will be printed out")
-		
+			print("only the result of square root of a will be printed out")
+
 		b = input("\ninput b=")
 		if f == "+":
 			answer = int(a) + int(b)
-			print ("\naddition=" +str(answer))
+			print("\naddition=" +str(answer))
 		elif f == "x" or f == "*":
 			answer = int(a) * int(b)
-			print ("\nmultiplication=" + str(answer))
+			print("\nmultiplication=" + str(answer))
 		elif f == "÷" or f == "/":
 			answer = int(a) / int(b)
-			print ("\ndivision=" + str(answer))
+			print("\ndivision=" + str(answer))
 		elif f == "-":
 			answer = int(a) - int(b)
-			print ("\nsubtraction=" + str(answer))
+			print("\nsubtraction=" + str(answer))
 		elif f == "**" or f == "xx":
 			answer = int(a) ** int(b)
-			print ("\nanswer=" + str(answer))
+			print("\nanswer=" + str(answer))
 		elif f == "sqrt":
 			import math
 			answer = math.sqrt(int(a))
-			print ("\nsquare root of a=" + str(answer))
-		
+			print("\nsquare root of a=" + str(answer))
+
 		if f != "sqrt":
-			print ("\nsolution=\n"+ a + f + b + "=" + 							str(answer))
-		print ("\nfinished calculation")
-		print ("\n\n\nanother calculation")
-		
+			print("\nsolution=\n"+ a + f + b + "=" + str(answer))
+			print("\nfinished calculation")
+			print("\n\n\nanother calculation")
+
 		continue
-		
+
 	except :
-		print ("\nEither/both a or b is/are not (an) integer(s) or incorrect function")
-		print ("\nPlease restart the programme \nCheck your input to make sure you follow the instruction carefully")
+		print("\nEither/both a or b is/are not (an) integer(s) or incorrect function")
+		print("\nPlease restart the programme \nCheck your input to make sure you follow the instruction carefully")
 		break
 
 # test for github demo
